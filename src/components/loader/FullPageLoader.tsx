10e9cc151edd18c5f92616d9f5b74efbf64e39b4
Olivier Deschênes <olivier.deschenes3@usherbrooke.ca>
2024-04-15 10:32:43 -0400

    fix

--- src/components/loader/FullPageLoader.tsx
+++ src/components/loader/FullPageLoader.tsx
@@ -1,11 +1,9 @@
+import { LoaderText } from './LoaderText';
+
 export const FullPageLoader = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="text-xl font-extrabold text-primary">
-        Loading <span className="animate-pulse">.</span>
-        <span className="animate-pulse">.</span>
-        <span className="animate-pulse">.</span>
-      </div>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <LoaderText />
     </div>
   );
 };
