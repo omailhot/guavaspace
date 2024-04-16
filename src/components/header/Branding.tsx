259039ef38d65d1209b661688d964f5ccbe0fc4a
omailhot <olivier.mailhot2@gmail.com>
2024-04-16 12:35:26 -0400

    Refactor branding translations and update header component

--- src/components/header/Branding.tsx
+++ src/components/header/Branding.tsx
@@ -1,13 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
 export const Branding = () => {
+  const { t } = useTranslation();
   return (
     <div className="my-8 hidden md:block md:w-[450px]">
       <h1 className="text-3xl font-bold text-primary md:text-4xl">
-        Trouvez un espace de travail qui vous convient.
+        {t('translation:branding.title')}
       </h1>
       <p className="mt-4">
-        Louez ou sous-louez des espaces de bureaux{' '}
-        <span className="text-primary">facilement</span> sur la plateforme de
-        location en ligne
+        {t('translation:branding.description1')}{' '}
+        <span className="text-primary">
+          {t('translation:branding.description2')}
+        </span>{' '}
+        {t('translation:branding.description3')}
       </p>
     </div>
   );
